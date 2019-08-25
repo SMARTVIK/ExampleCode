4827054e0b4393a3dde68f4f9e5d34776b7a2361
VIVEK <vps631532@gmail.com>
2019-08-25 21:38:48 +0530

    icons added

---
+++ app/src/main/java/com/spf/panditji/view/HomeActivity.kt
@@ -0,0 +1,13 @@
+package com.spf.panditji.view
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.spf.panditji.R
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
---
+++ app/src/main/java/com/spf/panditji/view/SplashActivity.kt
@@ -0,0 +1,17 @@
+package com.spf.panditji.view
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.spf.panditji.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_splash)
+
+
+
+    }
+}
